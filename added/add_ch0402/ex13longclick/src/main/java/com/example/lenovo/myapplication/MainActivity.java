115fab6614407ad3f73cf3faba88aa76c97763c5
kongxiangyue <513001363@qq.com>
2018-03-20 22:56:21 +0800

    [M]实现代码功能

--- added/add_ch0402/ex13longclick/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ added/add_ch0402/ex13longclick/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lenovo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(MainActivity.this
+                        , "长点击"
+                        , Toast.LENGTH_SHORT).show();
+
+
+                return false;
+            }
+        });
+
     }
 }
