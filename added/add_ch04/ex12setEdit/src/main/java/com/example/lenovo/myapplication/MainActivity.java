631c4378b1964642656c6a791a6be9fe3fc8ba86
kongxiangyue <513001363@qq.com>
2018-03-20 22:34:37 +0800

    [M] 实现功能代码

--- added/add_ch04/ex12setEdit/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ added/add_ch04/ex12setEdit/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.lenovo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+//第一步，在xml里添加三个控件
+//第二步，定义出三个控件对应的对象
+//第三步，绑定UI
+//第四步，设置OnClickListener
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = editText.getText().toString();
+
+                textView.setText(content);
+
+            }
+        });
+
+
     }
 }
