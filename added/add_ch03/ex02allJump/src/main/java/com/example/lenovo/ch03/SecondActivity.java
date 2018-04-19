823de9938d0b9cacdb794aa939def4446d57af71
kongxiangyue <513001363@qq.com>
2018-04-19 22:02:01 +0800

    [M]修复崩溃

--- added/add_ch03/ex02allJump/src/main/java/com/example/lenovo/ch03/SecondActivity.java
+++ added/add_ch03/ex02allJump/src/main/java/com/example/lenovo/ch03/SecondActivity.java
@@ -14,17 +14,22 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
-        Bundle bundle = getIntent().getExtras();
+        /*Bundle bundle = getIntent().getExtras();
 
         String key1 = bundle.getString("key1");
         int    key2 = bundle.getInt("key2");
 
         TextView tx3 = (TextView) findViewById(R.id.textView3);
 
-        tx3.setText("key1 的信息是："
-                + key1
-                + "   key2 的信息是："
-                + key2);
+        if (key1 != null) {
+            tx3.setText("key1 的信息是："
+                    + key1
+                    + "   key2 的信息是："
+                    + key2);
+        }*/
+
+
+
 
     }
 
