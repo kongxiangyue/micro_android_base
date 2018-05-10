e7d59f8ba4828d8aa01c2123d74fa2670c252b9e
kongxiangyue <513001363@qq.com>
2018-05-10 22:26:15 +0800

    [M]改成按键弹出对话框

--- code/ch05/dialogDemo/src/main/java/com/tarena/chapter5/dialogdemo/MainActivity.java
+++ code/ch05/dialogDemo/src/main/java/com/tarena/chapter5/dialogdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,24 +13,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Dialog Test"); /* 设置标题 */
-		builder.setMessage("这里是提示信息"); /* 设    置提示信息 */
-		builder.setIcon(R.drawable.ic_launcher); /* 加载图标 */
-		builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// “确定”按钮事件
-			}
-		});
-		builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// “取消”按钮事件
-			}
-		});
-		builder.create();
-		builder.show();
 
 	}
 
@@ -38,6 +21,32 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void alert(View v) {
+		//AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		//builder.setTitle("Dialog Test"); /* 设置标题 */
+		//builder.setMessage("这里是提示信息"); /* 设    置提示信息 */
+		//builder.setIcon(R.drawable.ic_launcher); /* 加载图标 */
+		//builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+		//	@Override
+		//	public void onClick(DialogInterface dialog, int which) {
+		//		// “确定”按钮事件
+		//	}
+		//});
+		//builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+		//	@Override
+		//	public void onClick(DialogInterface dialog, int which) {
+		//		// “取消”按钮事件
+		//	}
+		//});
+		//builder.create();
+		//builder.show();
+
+
+
+
+
 	}
 
 }
