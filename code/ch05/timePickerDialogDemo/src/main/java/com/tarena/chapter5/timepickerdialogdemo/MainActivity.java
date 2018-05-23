4a8989ab99e062f3179fd583b967dcb3932f7fd1
kongxiangyue <513001363@qq.com>
2018-05-23 09:37:29 +0800

    [M]规范填入参数

--- code/ch05/timePickerDialogDemo/src/main/java/com/tarena/chapter5/timepickerdialogdemo/MainActivity.java
+++ code/ch05/timePickerDialogDemo/src/main/java/com/tarena/chapter5/timepickerdialogdemo/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
 			public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
 				// TODO Auto-generated method stub
 				Toast.makeText(MainActivity.this, hourOfDay + ":" + minute,
-						3000).show();
+						Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
