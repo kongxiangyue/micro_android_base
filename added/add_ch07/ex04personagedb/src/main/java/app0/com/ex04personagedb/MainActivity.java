4d22136cb0edcf739e36c39de7fa69eb2757599b
kongxiangyue <513001363@qq.com>
2018-06-03 09:55:22 +0800

    [A]添加一个比较简易的SQLite例子

---
+++ added/add_ch07/ex04personagedb/src/main/java/app0/com/ex04personagedb/MainActivity.java
@@ -0,0 +1,13 @@
+package app0.com.ex04personagedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
