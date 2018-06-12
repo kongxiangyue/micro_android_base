4f3e2e3aee4a8b374b197e78e7dc19210bc36a2d
kongxiangyue <513001363@qq.com>
2018-06-12 10:21:09 +0800

    [M] 重命名,作为首界面的登录界面

--- training/MyAccount/app/src/main/java/com/example/kong/account/LoginMainActivity.java
+++ training/MyAccount/app/src/main/java/com/example/kong/account/LoginMainActivity.java
@@ -3,7 +3,7 @@ package com.example.kong.account;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
