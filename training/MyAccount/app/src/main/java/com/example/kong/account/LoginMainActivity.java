8b8dbc0f5f0a4c725fb3c4841504c2c61d3b1e69
kongxiangyue <513001363@qq.com>
2018-06-12 10:43:45 +0800

    [A] 登录功能 跳转

--- training/MyAccount/app/src/main/java/com/example/kong/account/LoginMainActivity.java
+++ training/MyAccount/app/src/main/java/com/example/kong/account/LoginMainActivity.java
@@ -1,13 +1,43 @@
 package com.example.kong.account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginMainActivity extends AppCompatActivity {
-
+    private Button btnLogin;
+    private Button btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin  = (Button) findViewById(R.id.btnLogin);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        
+        // 直接退出
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        //
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginMainActivity.this
+                        , MainPanelActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
---
+++ training/MyAccount/app/src/main/java/com/example/kong/account/MainPanelActivity.java
@@ -0,0 +1,14 @@
+package com.example.kong.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// 登录成功后主界面
+public class MainPanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_panel);
+    }
+}
